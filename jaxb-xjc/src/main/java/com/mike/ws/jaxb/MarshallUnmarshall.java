5f5ac9b7afa53c07489495efefd82312342219bb
Maichel Castro <maichel.castro@wnco.com>
2017-07-13 16:50:42 -0500

    Marshall Unmarshall example

---
+++ jaxb-xjc/src/main/java/com/mike/ws/jaxb/MarshallUnmarshall.java
@@ -0,0 +1,35 @@
+package com.mike.ws.jaxb;
+
+import java.io.StringReader;
+import java.io.StringWriter;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+
+import com.mike.ws.generated.v110.Mustang;
+
+public class MarshallUnmarshall {
+   
+   public static void main (String[] args){
+      try {
+         JAXBContext context = JAXBContext.newInstance(Mustang.class);
+         Marshaller marshaller = context.createMarshaller();
+         Mustang mustang = new Mustang();
+         mustang.setId(1);
+         mustang.setPlaca("RDG451");
+         StringWriter st = new StringWriter();
+         marshaller.marshal(mustang, st);
+         System.out.println(st.toString());
+         
+         Unmarshaller unmarshaller = context.createUnmarshaller();
+         Mustang unMustang = (Mustang) unmarshaller.unmarshal(new StringReader(st.toString()));
+         System.out.println(unMustang.getPlaca());
+      } catch (JAXBException e) {
+         e.printStackTrace();
+      }
+      
+   }
+
+}
